6986ef64acd4adbdbf4090536812760a763f7061
1939323749 <1939323749@qq.com>
2023-11-25 13:11:24 +0800

    feat: new listening check answer experience

--- app/src/main/java/app/xlei/vipexam/ui/question/listening/listening.kt
+++ app/src/main/java/app/xlei/vipexam/ui/question/listening/listening.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -20,7 +21,6 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.xlei.vipexam.data.Muban
 import app.xlei.vipexam.data.Shiti
@@ -72,13 +72,14 @@ private fun listening(
     showAnswer: MutableState<Boolean>,
 ){
     val choices by remember { mutableStateOf(getListeningChoices(muban.shiti)) }
-    val midiaPlayers = mutableListOf<Pair<MediaPlayer,String>>()
+    val mediaPlayers = mutableListOf<Pair<MediaPlayer,String>>()
     muban.shiti.forEach {
-        midiaPlayers.add(remember { MediaPlayer() } to it.audioFiles)
+        mediaPlayers.add(remember { MediaPlayer() } to it.audioFiles)
     }
 
     val scrollState = rememberLazyListState()
     val firstVisibleItemIndex by remember { derivedStateOf { scrollState.firstVisibleItemIndex } }
+    val tooltipStates = getListeningToolTipStates(muban.shiti)
     val coroutine = rememberCoroutineScope()
     val haptics = LocalHapticFeedback.current
 
@@ -86,14 +87,14 @@ private fun listening(
         coroutine.launch {
             delay(500)
             withContext(Dispatchers.IO){
-                midiaPlayers.forEach{
+                mediaPlayers.forEach{
                     it.first.setDataSource("https://rang.vipexam.org/Sound/${it.second}.mp3")
                     it.first.prepare()
                 }
             }
         }
         onDispose {
-            midiaPlayers.forEach {
+            mediaPlayers.forEach {
                 it.first.release()
             }
         }
@@ -118,61 +119,24 @@ private fun listening(
             }
 
             items(muban.shiti.size) {
-                Text((it+1).toString())
-                AudioPlayer(midiaPlayers[it].first)
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(12.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                        .background(MaterialTheme.colorScheme.primaryContainer)
-                        .clickable {
-                            //onQuestionClick()
-                            showOptionsSheet.value = true
-                            selectedChoiceIndex.value= it to 0
-                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                        }
-                ) {
-                    Column (
-                        modifier = Modifier
-                            .padding(12.dp)
-                    ){
-                        Text(
-                            text = "${1}",
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                        Text(
-                            text = "[A]" + muban.shiti[it].first,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                        Text(
-                            text = "[B]" + muban.shiti[it].second,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                        Text(
-                            text = "[C]" + muban.shiti[it].third,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                        Text(
-                            text = "[D]" + muban.shiti[it].fourth,
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                    }
-
-                    if(getChoice(it,0,choices) !=null){
-                        if (getChoice(it,0,choices)!!.value.second.second!=null){
-                            SuggestionChip(
-                                onClick = {
-                                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                                },
-                                label = {
-                                    getChoice(it,0,choices)!!.value.second.second?.let { choice-> Text(choice) }
+                Text("${it + 1}")
+                AudioPlayer(mediaPlayers[it].first)
+                TooltipBox(
+                    positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                    tooltip = {
+                        if (showAnswer.value)
+                            RichTooltip(
+                                title = { Text("1" + muban.shiti[0].refAnswer) },
+                                modifier = Modifier
+                                    .padding(top = 100.dp, bottom = 100.dp)
+                            ){
+                                LazyColumn {
+                                    item { Text(muban.shiti[it].originalText) }
                                 }
-                            )
-                        }
-                    }
-                }
-                for ((n,t) in muban.shiti[it].children.withIndex()) {
+                            }
+                    },
+                    state = getListeningToolTipState(it,0,tooltipStates)!!.second.second
+                ) {
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -180,8 +144,9 @@ private fun listening(
                             .clip(RoundedCornerShape(12.dp))
                             .background(MaterialTheme.colorScheme.primaryContainer)
                             .clickable {
+                                //onQuestionClick()
                                 showOptionsSheet.value = true
-                                selectedChoiceIndex.value = it to n+1
+                                selectedChoiceIndex.value= it to 0
                                 haptics.performHapticFeedback(HapticFeedbackType.LongPress)
                             }
                     ) {
@@ -190,8 +155,8 @@ private fun listening(
                                 .padding(12.dp)
                         ){
                             Text(
-                                text = "${n + 2}",
-                                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                                text = "${1}",
+                                color = MaterialTheme.colorScheme.onPrimaryContainer
                             )
                             Text(
                                 text = "[A]" + muban.shiti[it].first,
@@ -211,16 +176,91 @@ private fun listening(
                             )
                         }
 
-                        if(getChoice(it,n+1,choices) !=null){
-                            if (getChoice(it,n+1,choices)!!.value.second.second!=null){
+                        if(getChoice(it,0,choices) !=null){
+                            if (getChoice(it,0,choices)!!.value.second.second!=null){
                                 SuggestionChip(
                                     onClick = {
                                         haptics.performHapticFeedback(HapticFeedbackType.LongPress)
                                     },
                                     label = {
-                                        getChoice(it,n+1,choices)!!.value.second.second?.let { choice-> Text(choice) }
+                                        getChoice(it,0,choices)!!.value.second.second?.let { choice-> Text(choice) }
                                     }
                                 )
+                            }
+                        }
+                    }
+                }
+
+                for ((n,t) in muban.shiti[it].children.withIndex()) {
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                        tooltip = {
+                            if (showAnswer.value)
+                                RichTooltip(
+                                    title = { Text("${n  + 2}" + t.refAnswer) },
+                                    modifier = Modifier
+                                        .padding(top = 100.dp, bottom = 100.dp)
+                                ){
+                                    LazyColumn {
+                                        item { Text(muban.shiti[it].originalText) }
+                                    }
+                                }
+                        },
+                        state = getListeningToolTipState(it, n + 1 ,tooltipStates)!!.second.second,
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp)
+                                .clip(RoundedCornerShape(12.dp))
+                                .background(MaterialTheme.colorScheme.primaryContainer)
+                                .clickable {
+                                    showOptionsSheet.value = true
+                                    selectedChoiceIndex.value = it to n + 1
+                                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                }
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .padding(12.dp)
+                            ) {
+                                Text(
+                                    text = "${n + 2}",
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                                )
+                                Text(
+                                    text = "[A]" + t.first,
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                                Text(
+                                    text = "[B]" + t.second,
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                                Text(
+                                    text = "[C]" + t.third,
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                                Text(
+                                    text = "[D]" + t.fourth,
+                                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+
+                            if (getChoice(it, n + 1, choices) != null) {
+                                if (getChoice(it, n + 1, choices)!!.value.second.second != null) {
+                                    SuggestionChip(
+                                        onClick = {
+                                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                        },
+                                        label = {
+                                            getChoice(it, n + 1, choices)!!.value.second.second?.let { choice ->
+                                                Text(
+                                                    choice
+                                                )
+                                            }
+                                        }
+                                    )
+                                }
                             }
                         }
                     }
@@ -230,14 +270,7 @@ private fun listening(
 
 
 
-            if(showAnswer.value)
-                items(muban.shiti.size){
-                    Text("1"+muban.shiti[it].refAnswer)
-                    for((no,children) in muban.shiti[it].children.withIndex()){
-                        Text("${no + 2}"+children.refAnswer)
-                    }
-                    Text(muban.shiti[it].originalText)
-                }
+
         }
 
 
@@ -328,4 +361,35 @@ fun AudioPlayer(
             Text(if (isPlaying) "Pause" else "Play")
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun getListeningToolTipStates(shiti: List<Shiti>): MutableList<Pair<Int, Pair<Int, TooltipState>>> {
+    val toolTipStates = mutableListOf<Pair<Int,Pair<Int,TooltipState>>>()
+
+    for((index,ti) in shiti.withIndex()){
+        toolTipStates.add(
+            index to (0 to rememberTooltipState(isPersistent = true))
+        )
+        for ((i,_) in ti.children.withIndex()){
+            toolTipStates.add(
+                index to (i+1 to rememberTooltipState(isPersistent = true))
+            )
+        }
+    }
+
+    return toolTipStates
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+fun getListeningToolTipState(index:Int, i:Int, toolTipStates: MutableList<Pair<Int, Pair<Int, TooltipState>>>): Pair<Int, Pair<Int, TooltipState>>? {
+    for(toolTipState in toolTipStates){
+        if(toolTipState.first==index){
+            if(toolTipState.second.first==i){
+                return toolTipState
+            }
+        }
+    }
+    return null
 }
